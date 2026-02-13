163f96bdc131b90e542d2fa9f46378d6b56724d2
potatameister <180633607+potatameister@users.noreply.github.com>
2026-02-13 22:15:39 +0700

    fix: sync dashboard with android project in build pipeline

--- dashboard/capacitor.config.ts
+++ dashboard/capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.potatameister.smapi',
   appName: 'PotataSMAPI',
-  webDir: 'dist'
+  webDir: 'dist',
+  android: {
+    path: '../android'
+  }
 };
 
 export default config;
