59c85e1c2a9b202068c0c27167a17eb1d9c63cf8
potatameister <180633607+potatameister@users.noreply.github.com>
2026-02-22 21:27:21 +0700

    Fix Bundle parameter in onActivitySaveInstanceState

--- android/app/src/main/java/io/potatasmapi/launcher/PotataApp.kt
+++ android/app/src/main/java/io/potatasmapi/launcher/PotataApp.kt
@@ -92,7 +92,7 @@ class PotataApp : Application() {
             override fun onActivityResumed(activity: Activity) {}
             override fun onActivityPaused(activity: Activity) {}
             override fun onActivityStopped(activity: Activity) {}
-            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle?) {}
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
             override fun onActivityDestroyed(activity: Activity) {}
         })
 
